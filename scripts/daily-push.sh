4d0d6964f28ccd5f20c3aafc0db8cd8615ab4714
Abhinesh <abhineshgarhwal@gmail.com>
2023-01-07 17:30:52 +0000

    Update bash script

--- scripts/daily-push.sh
+++ scripts/daily-push.sh
@@ -1,6 +1,8 @@
 # Bash script to fetch submissions from leetcode.com and push it to github
 
 # Run python3 script to fetch recent subsmissons
+cd ~/github/LeetCode-Solutions/scripts/
+
 echo -e "\n\n" >> leetcode-submissions-downloader.log
 python3 leetcode-submissions-downloader.py >> leetcode-submissions-downloader.log
 
@@ -9,14 +11,9 @@ python3 leetcode-submissions-downloader.py >> leetcode-submissions-downloader.lo
 cd ../
 
 git add scripts/*.log
-
 git add Solutions/
-
 today=`date`
-
 git commit -m "Submission added on $today"
-
 git push
-
 
 echo -e "'git push' Completed\n\n" >> scripts/leetcode-submissions-downloader.log
