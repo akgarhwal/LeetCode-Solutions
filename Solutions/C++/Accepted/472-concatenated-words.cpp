8777ddf69c22e989164692a474f381e11feabc3c
Abhinesh <abhineshgarhwal@gmail.com>
2023-01-28 05:40:32 +0530

    Submission added on Sat Jan 28 00:05:31 UTC 2023 (#20)

---
+++ Solutions/C++/Accepted/472-concatenated-words.cpp
@@ -0,0 +1,47 @@
+/*
+Problem Title: 472 - Concatenated Words
+Lang: C++
+Runtime: 184 ms
+Memory: 39.9 MB
+Problem Url: https://leetcode.com/problems/concatenated-words
+Submission Url: https://leetcode.com/problems/concatenated-words/submissions/886049442
+*/
+
+class Solution {
+public:
+
+    bool solve(int count, string word, unordered_set<string>& ht) {
+        if(word.size() == 0){
+            return true and  count > 1;
+        }
+        // cout<<word<<endl;
+        bool res = false;
+        string prefix = "";
+        for(int i=0; i < word.size(); i++) {
+            prefix += word[i];
+            if(ht.find(prefix) != ht.end()) {
+                res = res or solve(count+1, word.substr(i+1), ht);
+            }
+            // cout<<" ==== \n";
+        }
+        return res;
+    } 
+
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> ht;
+        for(string word : words){
+            ht.insert(word);
+        }
+
+        vector<string> result;
+
+        for(string word : words){
+            // cout<<" -------- \n";
+            bool res = solve(0, word, ht);
+            if(res){
+                result.push_back(word);
+            }
+        }
+        return result;
+    }
+};
