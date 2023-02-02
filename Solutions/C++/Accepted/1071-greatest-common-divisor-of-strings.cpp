d7a24e1816ea67aa104815abf12cbcb0c54c139d
Abhinesh <abhineshgarhwal@gmail.com>
2023-02-02 00:05:34 +0000

    Submission added on Thu Feb  2 00:05:34 UTC 2023

---
+++ Solutions/C++/Accepted/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,42 @@
+/*
+Problem Title: 1071 - Greatest Common Divisor of Strings
+Lang: C++
+Runtime: 36 ms
+Memory: 25.3 MB
+Problem Url: https://leetcode.com/problems/greatest-common-divisor-of-strings
+Submission Url: https://leetcode.com/problems/greatest-common-divisor-of-strings/submissions/889081499
+*/
+
+class Solution {
+public:
+    bool isDivideStr(string str, string x){
+        for(int i=0; i <str.size(); i++) {
+            if(str[i] != x[(i%(int(x.size())))]){
+                return false;
+            }
+        }
+        return true and ((int)str.size())%(int(x.size())) == 0;
+    }
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 == str2){
+            return str1;
+        }
+        string ans = "";
+        char ch = str1[0];
+        int index = str1.find(ch);
+        while(index >= 0) {
+
+            string x = str1.substr(0,index);
+            if(x.size() > 0 and isDivideStr(str1, x) and isDivideStr(str2, x)) {
+                ans = x;
+            }
+            int newIndex = str1.substr(index+1).find(ch);
+            if(newIndex < 0) {
+                break;
+            }
+            index = index + 1 + newIndex;
+        }
+
+        return ans;
+    }
+};
