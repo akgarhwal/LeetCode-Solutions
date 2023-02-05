f9ba8150a7668e8fb968535adb0de0cbe7270b1d
Abhinesh <abhineshgarhwal@gmail.com>
2023-02-05 00:05:34 +0000

    Submission added on Sun Feb  5 00:05:34 UTC 2023

---
+++ Solutions/C++/Wrong-Answer/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,35 @@
+/*
+Problem Title: 1346 - Check If N and Its Double Exist
+Lang: C++
+Runtime: N/A
+Memory: N/A
+Problem Url: https://leetcode.com/problems/check-if-n-and-its-double-exist
+Submission Url: https://leetcode.com/problems/check-if-n-and-its-double-exist/submissions/890999114
+*/
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        bool ans = false;
+        for(int i = 0 ; i < arr.size(); i++) {
+            int lo  = 0, hi = arr.size()-1, target = arr[i]*2;
+            while(lo <= hi) {
+                int mid = lo + ((hi-lo)>>1);
+                if(arr[mid] == target) {
+                    ans = true;
+                    break;
+                }
+                if(arr[mid] > target) {
+                    hi = mid -1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            if(ans){
+                break;
+            }
+        }
+        return ans;
+    }
+};
