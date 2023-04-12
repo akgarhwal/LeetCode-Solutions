1513955a5206b4217233111526403fb80aa065e3
Abhinesh <abhineshgarhwal@gmail.com>
2023-04-12 06:14:56 +0530

    Submission added on Wed Apr 12 00:05:33 UTC 2023 (#95)

---
+++ Solutions/C++/Accepted/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,34 @@
+/*
+Problem Title: 2390 - Removing Stars From a String
+Lang: C++
+Runtime: 96 ms
+Memory: 27.1 MB
+Problem Url: https://leetcode.com/problems/removing-stars-from-a-string
+Submission Url: https://leetcode.com/problems/removing-stars-from-a-string/submissions/931603618
+*/
+
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '*') {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+
+        string ans = "";
+
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
