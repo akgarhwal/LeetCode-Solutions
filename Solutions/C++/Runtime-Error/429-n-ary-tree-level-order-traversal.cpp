420301a12de4d1290215ebd077dd5fae20696d6d
Abhinesh <abhineshgarhwal@gmail.com>
2023-05-10 07:08:40 +0530

    Submission added on Wed May 10 00:05:34 UTC 2023 (#115)

---
+++ Solutions/C++/Runtime-Error/429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,59 @@
+/*
+Problem Title: 429 - N-ary Tree Level Order Traversal
+Lang: C++
+Runtime: N/A
+Memory: N/A
+Problem Url: https://leetcode.com/problems/n-ary-tree-level-order-traversal
+Submission Url: https://leetcode.com/problems/n-ary-tree-level-order-traversal/submissions/947266255
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        
+        vector<vector<int>> ans;
+
+        int level = 0;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+
+        while(!q.empty()) {
+
+            auto ele = q.front(); q.pop();
+
+            if(ele.second >= ans.size()) {
+                vector<int> temp;
+                temp.push_back(ele.first->val);
+                ans.push_back(temp);
+            } else {
+                ans[ele.second].push_back(ele.first->val);
+            }
+
+            for(auto child : ele.first->children) {
+                q.push({child, ele.second + 1});
+            }
+        }
+
+        return ans;
+    }
+};
